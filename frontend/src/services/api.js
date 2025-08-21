ddc40378fde2ed3c3161ddc829df6b55905a7d5a
92Mohammad <m.e89571828@gmail.com>
2025-08-21 11:14:50 +0530

    fronted working well

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
-const API_BASE_URL = 'https://school-management-api-0dyr.onrender.com/';
+const API_BASE_URL = 'http://localhost:3000/';
 
 export const apiService = {
   // Add a new school
   async addSchool(schoolData) {
     try {
-      const response = await fetch(`${API_BASE_URL}/api/addSchool`, {
+      const response = await fetch(`${API_BASE_URL}addSchool`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -32,7 +32,7 @@ export const apiService = {
   async getSchoolsByProximity(latitude, longitude) {
     try {
       const response = await fetch(
-        `${API_BASE_URL}/api/listSchools?latitude=${latitude}&longitude=${longitude}`,
+        `${API_BASE_URL}listSchools?latitude=${latitude}&longitude=${longitude}`,
         {
           method: 'GET',
           headers: {
